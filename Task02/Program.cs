9d91fc98ec7ef9335865025fa21a0c7cc724704a
NKompa <nkompa7@gmail.com>
2022-09-22 13:22:19 +0300

    Get sum of numbers from M to N

---
+++ Task02/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetSum (int start, int end)
+{
+    if (start > end) return 0;
+    else return start + GetSum(start+1,end); 
+}
+Console.WriteLine(GetSum(2,8));
