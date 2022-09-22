13e3fca6838cb99ad9b720a743f4ac38b76ad365
NKompa <nkompa7@gmail.com>
2022-09-22 13:08:27 +0300

    Get natural numbers from M to N

---
+++ Task01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string NuturalNumbers (int start, int end)
+{
+    if (start<=end) return $"{start} " + NuturalNumbers(start+1,end);
+    else return string.Empty;
+}
+Console.WriteLine(NuturalNumbers(1,8));
